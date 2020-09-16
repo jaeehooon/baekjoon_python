e4ebaf17e7c58c87b72b4fe04deb969dc2161a3e
jaehooon <qlenfr0922@gmail.com>
2020-09-16 22:47:03 +0900

    [10996] 200916 22:47

--- 단계별로 풀어보기/5. 실습 1/[10996] 별 찍기 - 21.py
+++ 단계별로 풀어보기/5. 실습 1/[10996] 별 찍기 - 21.py
@@ -1,20 +1,31 @@
 import sys
 
-
-def _rotate(num):
-    for i in range(1, num+1):
-        print("{}".format("*" if i % 2 == 1 else " "), end='')
-    print()
-    for i in range(1, num+1):
-        print("{}".format("*" if i % 2 == 0 else " "), end='')
-
-
-def rotate(num):
-    for i in range(1, num+1):
-        _rotate(num)
-        print()
+"""
+다른 사람 풀이
+"""
+number = int(sys.stdin.readline())
+for n in range(1, number+1):
+    print("* " * ((number + 1) // 2))
+    print(" *" * (number // 2))
 
 
-if __name__ == "__main__":
-    number = int(sys.stdin.readline())
-    rotate(number)
+"""
+내가 푼 풀이
+"""
+# def _rotate(num):
+#     for i in range(1, num+1):
+#         print("{}".format("*" if i % 2 == 1 else " "), end='')
+#     print()
+#     for i in range(1, num+1):
+#         print("{}".format("*" if i % 2 == 0 else " "), end='')
+#
+#
+# def rotate(num):
+#     for i in range(1, num+1):
+#         _rotate(num)
+#         print()
+#
+#
+# if __name__ == "__main__":
+#     number = int(sys.stdin.readline())
+#     rotate(number)

939696c7ee2120c608a98355fb80829fd93d20d3
jaehooon <qlenfr0922@gmail.com>
2020-09-17 12:44:58 +0900

    [11720] 200917 12:44

--- 단계별로 풀어보기/8. 문자열/[11720] 숫자의 합.py
+++ 단계별로 풀어보기/8. 문자열/[11720] 숫자의 합.py
@@ -1,9 +1,13 @@
 import sys
 
+"""
+굳이 처음에 입력받은 숫자를 사용하려면
+슬라이싱 정도..?
+"""
 
 num = int(sys.stdin.readline())
 number = sys.stdin.readline().rstrip('\n')
 sum_ = 0
-for i in number:
+for i in number[:num]:
     sum_ += int(i)
 print(sum_)

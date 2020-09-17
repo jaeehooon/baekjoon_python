9b43a8de8eabeea3b3dff13995e27902a6f39edd
jaehooon <qlenfr0922@gmail.com>
2020-09-17 15:42:04 +0900

    [2751] 200917 15:41

--- 단계별로 풀어보기/12. 정렬/[2751] 수 정렬하기 2.py
+++ 단계별로 풀어보기/12. 정렬/[2751] 수 정렬하기 2.py
@@ -1,6 +1,9 @@
 import sys
 
 
+"""
+내가 푼 풀이
+"""
 num = int(sys.stdin.readline())
 num_list = list()
 for _ in range(num):
@@ -9,3 +12,15 @@ for _ in range(num):
 sorted_list = sorted(num_list)
 for i in sorted_list:
     print(i)
+
+
+"""
+다른 사람 풀이
+- stdout을 통해 출력
+- 파이참에서는 입력받는 부분에서 계속 입력을 받게됨...
+"""
+from sys import stdin, stdout
+
+input()
+arr = sorted(map(int, stdin.read().split()))
+stdout.write('\n'.join(map(str,arr)))

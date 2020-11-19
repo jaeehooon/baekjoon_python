6852b91f1c72dc4fbd04f5076e975524717c007c
jaehooon <qlenfr0922@gmail.com>
2020-11-19 11:53:00 +0900

    [10989] 201119 11:52

--- 단계별로 풀어보기/12. 정렬/[10989] 수 정렬하기 3.py
+++ 단계별로 풀어보기/12. 정렬/[10989] 수 정렬하기 3.py
@@ -2,6 +2,28 @@ import sys
 
 
 num = int(sys.stdin.readline())
-num_list = [int(sys.stdin.readline()) for _ in range(num)]
-for i in sorted(num_list):
-    print(i)
+result = [0] * 10001
+for _ in range(num):
+    n = int(sys.stdin.readline())
+    result[n] += 1
+
+"""
+내가 푼 풀이
+"""
+for i in range(10001):
+    if result[i] != 0:
+
+        if result[i] != 1:
+            for j in range(result[i]):
+                print(i)
+        else:
+            print(i)
+
+
+"""
+문자열 출력 풀이
+"""
+
+for i, n in enumerate(result):
+    if n >= 1:
+        print("{}\n".format(i) * n, end='')
